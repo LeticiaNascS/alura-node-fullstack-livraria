d8f0c41a39897014e9e6a6ef7c9a7f284ca054f6
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-02-13 15:49:38 -0300

    feat: Integrate header and content sections into main application

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Header from './componentes/header';
+import  UltimosLancamentos from './componentes/ultimosLancamentos';  
+
+function App(){
+  return (
+    <div className="App">
+      <Header />
+      <UltimosLancamentos/>
+    </div>
+  );
+}
+
+export default App;
