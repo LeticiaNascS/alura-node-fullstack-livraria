677010efdfa877706fd892c25ab5a4a555d61e61
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-02-13 15:50:47 -0300

    style: Refine styling for common UI components

--- src/componentes/input/index.js
+++ src/componentes/input/index.js
@@ -1,29 +1,20 @@
-import styled from "styled-components"; 
+import styled from "styled-components";
 
 const Input = styled.input`
-        display: center;
-        border: 1px solid #1c0f0fe1;
-        background: transparent;
-        border: 1px solid #6B8E6E;
-        border-radius: 30px;
-        padding: 10px;
-        width: 110%;
-        color: #000000;
-        font-size: 16px;
-        margin-bottom: 10px;
+  order: 1px solid #fff;
+  background: transparent;
+  border: 1px solid #fff;
+  padding: 20px 140px;
+  border-radius: 50px;
+  width: 200px;
+  color: #fff;
+  font-size: 16px;
+  margin-bottom: 30px;
 
-        &::placeholder {
-                color: #231616;
-                font-size: 16px;
-                text-align: center;
-                
-        }
+  &::placeholder {
+    color: #fff;
+    font-size: 16px;
+  }
+`;
 
-
-`
-
-export default Input
-
-
-
-
+export default Input;
