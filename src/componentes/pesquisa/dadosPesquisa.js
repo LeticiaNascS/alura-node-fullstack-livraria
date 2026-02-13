dd34676fdcfc012bcd72a2e8b016e27fc4382d13
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-02-13 15:50:47 -0300

    feat: Implement dynamic search functionality with sample book data

--- src/componentes/pesquisa/dadosPesquisa.js
+++ src/componentes/pesquisa/dadosPesquisa.js
@@ -1,12 +1,9 @@
+import livro from "../../Imagens/livro.png";
 
-import imgcurso from '../../Imagens/livro.png';
-
-
-
- export const Lista = [
-  //  {nome: 'Buque de Rosas',id:1, src:imgcurso  },
-  //  {nome: 'Buque de Lírios',id:2, src:imgcurso  },
-    {nome: 'Buque de Girassóis',id:3, src:imgcurso },
-  //  {nome: 'Buque de Rosas do Campo',id:4, src:imgcurso  },
-  //  {nome: 'Buque de Rosas Brancas',id:5, src:imgcurso }
-]; 
+export const livros = [
+  { nome: "Liderança em Design", id: 1, src: livro },
+  { nome: "Liderança em UX", id: 1, src: livro },
+  { nome: "JavaScript Assertivo", id: 2, src: livro },
+  { nome: "Cangaceiro em JavaScript", id: 3, src: livro },
+  { nome: "Aventureiros do Java", id: 4, src: livro },
+];
--- src/componentes/pesquisa/index.js
+++ src/componentes/pesquisa/index.js
@@ -1,34 +1,75 @@
-import Input from '../input';
-import styled from 'styled-components';
-import { useState } from 'react';
-import { Lista } from '../pesquisa/dadosPesquisa';
+import Input from "../input";
+import styled from "styled-components";
+import { useState } from "react";
+import { livros } from "./dadosPesquisa";
 
-const PesquisarContainer = styled.section``;
-function Pesquisar (){
-    const [pesquisaDigitado, setPesquisaDigitado] = useState([ ])
-    return(
-        <PesquisarContainer> 
-    <Input
-        placeholder="Pesquise por um produto"    
-        onBlur={evento => {
-            const textoDigitado = evento.target.value
-            const resultadoPesquisa = Lista.filter(Lista => Lista.nome.includes (textoDigitado))
-            setPesquisaDigitado(resultadoPesquisa)
+const PesquisaContainer = styled.section`
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+  color: #fff;
+  text-align: center;
+  padding: 85px 0;
+  min-height: 470px;
+  width: 100%;
+`;
+
+const Titulo = styled.h2`
+  color: #fff;
+  font-size: 36px;
+  text-align: center;
+  width: 100%;
+`;
+
+const Subtitulo = styled.h3`
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 40px;
+`;
+
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+
+  p {
+    width: 200px;
+  }
+
+  img {
+    width: 100px;
+  }
+
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
+function Pesquisa() {
+  const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+
+  return (
+    <PesquisaContainer>
+      <Titulo>Já sabe por onde começar?</Titulo>
+      <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
+      <Input
+        placeholder="Escreva sua próxima leitura"
+        onBlur={(evento) => {
+          const textoDigitado = evento.target.value;
+          const resultadoPesquisa = livros.filter((livro) =>
+            livro.nome.includes(textoDigitado),
+          );
+          setLivrosPesquisados(resultadoPesquisa);
         }}
-        />
-        {pesquisaDigitado.map (Lista => (
-            <div> 
-                <p>{Lista.nome}</p>
-            <img src= {Lista.src} 
-            style={{ width: '60px', height: 'auto' }}
-            ></img>
-       
-            </div>
-     ))}
-        </PesquisarContainer>   
-
-       
-
-    );
+      />
+      {livrosPesquisados.map((livro) => (
+        <Resultado>
+          <img src={livro.src} alt={livro.nome} />
+          <p>{livro.nome}</p>
+        </Resultado>
+      ))}
+    </PesquisaContainer>
+  );
 }
-export default Pesquisar
+
+export default Pesquisa;
